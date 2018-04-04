479e9cb5b9ff39094a34de0e2828b93a3d96f7fd
njligames <jamesfolk1@gmail.com>
2018-04-04 11:12:30 -0400

    added oculus rift libraries

--- cmake/oculus_windows32_external_libraries.cmake
+++ cmake/oculus_windows32_external_libraries.cmake
@@ -1,6 +1,9 @@
 
 message(STATUS "Platform is oculus windows32 for engine")
 
+# -------------------------------------------------------------------------------------------------------------------------
+# SDL2
+
 set(${CMAKE_PROJECT_NAME}_URL_SDL2_INCLUDE 
   "https://www.dropbox.com/s/g0qrc1xkz2qcb9y/SDL2-2.0.7_include.tar.gz?dl=0" 
   CACHE STRING "The URL for the include files for SDL2")
@@ -22,7 +25,7 @@ RETRIEVE_TAR(
 include_directories("${CMAKE_BINARY_DIR}/${SDL2_INCLUDE_DIR}")
 
 set(LIBSDL_LIBRARY_URL "${${CMAKE_PROJECT_NAME}_URL_SDL2_LIB_WINDOWS_WIN32_RELEASE}")
-set(LIBSDL_LIBRARY_TARGET_DIRECTORY "thirdparty/SDL2-2.0.7/lib/windows/x86")
+set(LIBSDL_LIBRARY_TARGET_DIRECTORY "thirdparty/SDL2-2.0.7/lib/windows/Win32")
 
 RETRIEVE_TAR(
   "${LIBSDL_LIBRARY_URL}"
@@ -40,3 +43,41 @@ set(SDLMAIN_IMPORT_LOCATION "${CMAKE_BINARY_DIR}/${LIBSDL_LIBRARY_TARGET_DIRECTO
 add_library(SDL2MAIN_LIBRARY STATIC IMPORTED )
 set_target_properties(SDL2MAIN_LIBRARY PROPERTIES IMPORTED_LOCATION "${SDLMAIN_IMPORT_LOCATION}")
 list(APPEND EXTRA_LIBS SDL2MAIN_LIBRARY)
+
+# -------------------------------------------------------------------------------------------------------------------------
+# Oculus
+
+set(${CMAKE_PROJECT_NAME}_URL_OCULUS_INCLUDE 
+  "https://www.dropbox.com/s/93t7xb74oj83agl/LIBOVR_include.tar.gz?dl=0" 
+  CACHE STRING "The URL for the include files for Oculus Rift")
+
+set(${CMAKE_PROJECT_NAME}_URL_OCULUS_LIB_WINDOWS_WIN32_RELEASE 
+  "https://www.dropbox.com/s/8p2pca0tb9tx8pw/LIBOVR_windows_win32.tar.gz?dl=0" 
+  CACHE STRING "The URL for the Windows 32bit version of Oculus Rift"
+  )
+
+set(LIBOVR_INCLUDE_URL "${${CMAKE_PROJECT_NAME}_URL_OCULUS_INCLUDE}")
+set(LIBOVR_INCLUDE_DIR "thirdparty/LibOVR/include")
+
+RETRIEVE_TAR(
+  "${LIBOVR_INCLUDE_URL}"
+  "${LIBOVR_INCLUDE_DIR}"
+  "SKIP"
+  )
+
+include_directories("${CMAKE_BINARY_DIR}/${LIBOVR_INCLUDE_DIR}")
+
+set(LIBOVR_LIBRARY_URL "${${CMAKE_PROJECT_NAME}_URL_OCULUS_LIB_WINDOWS_WIN32_RELEASE}")
+set(LIBOVR_LIBRARY_TARGET_DIRECTORY "thirdparty/LibOVR/lib/windows/Win32")
+
+RETRIEVE_TAR(
+  "${LIBOVR_LIBRARY_URL}"
+  "${LIBOVR_LIBRARY_TARGET_DIRECTORY}"
+  "SKIP"
+  )
+
+set(LIBOVR_IMPORT_LOCATION "${CMAKE_BINARY_DIR}/${LIBOVR_LIBRARY_TARGET_DIRECTORY}/LibOVR.${STATIC_LIBRARY_EXTENSION}")
+add_library(LIBOVR_LIBRARY STATIC IMPORTED )
+set_target_properties(LIBOVR_LIBRARY PROPERTIES IMPORTED_LOCATION "${LIBOVR_IMPORT_LOCATION}")
+list(APPEND EXTRA_LIBS LIBOVR_LIBRARY)
+
--- cmake/oculus_windows64_external_libraries.cmake
+++ cmake/oculus_windows64_external_libraries.cmake
@@ -1,6 +1,9 @@
 
 message(STATUS "Platform is oculus windows64 for engine")
 
+# -------------------------------------------------------------------------------------------------------------------------
+# SDL2
+
 set(${CMAKE_PROJECT_NAME}_URL_SDL2_INCLUDE 
   "https://www.dropbox.com/s/g0qrc1xkz2qcb9y/SDL2-2.0.7_include.tar.gz?dl=0" 
   CACHE STRING "The URL for the include files for SDL2")
@@ -40,3 +43,41 @@ set(SDLMAIN_IMPORT_LOCATION "${CMAKE_BINARY_DIR}/${LIBSDL_LIBRARY_TARGET_DIRECTO
 add_library(SDL2MAIN_LIBRARY STATIC IMPORTED )
 set_target_properties(SDL2MAIN_LIBRARY PROPERTIES IMPORTED_LOCATION "${SDLMAIN_IMPORT_LOCATION}")
 list(APPEND EXTRA_LIBS SDL2MAIN_LIBRARY)
+
+# -------------------------------------------------------------------------------------------------------------------------
+# Oculus
+
+set(${CMAKE_PROJECT_NAME}_URL_OCULUS_INCLUDE 
+  "https://www.dropbox.com/s/93t7xb74oj83agl/LIBOVR_include.tar.gz?dl=0" 
+  CACHE STRING "The URL for the include files for Oculus Rift")
+
+set(${CMAKE_PROJECT_NAME}_URL_OCULUS_LIB_WINDOWS_X64_RELEASE 
+  "https://www.dropbox.com/s/vnshsaoz52hy877/LIBOVR_windows_win64.tar.gz?dl=0" 
+  CACHE STRING "The URL for the Windows 32bit version of Oculus Rift"
+  )
+
+set(LIBOVR_INCLUDE_URL "${${CMAKE_PROJECT_NAME}_URL_OCULUS_INCLUDE}")
+set(LIBOVR_INCLUDE_DIR "thirdparty/LibOVR/include")
+
+RETRIEVE_TAR(
+  "${LIBOVR_INCLUDE_URL}"
+  "${LIBOVR_INCLUDE_DIR}"
+  "SKIP"
+  )
+
+include_directories("${CMAKE_BINARY_DIR}/${LIBOVR_INCLUDE_DIR}")
+
+set(LIBOVR_LIBRARY_URL "${${CMAKE_PROJECT_NAME}_URL_OCULUS_LIB_WINDOWS_X64_RELEASE}")
+set(LIBOVR_LIBRARY_TARGET_DIRECTORY "thirdparty/LibOVR/lib/windows/x64")
+
+RETRIEVE_TAR(
+  "${LIBOVR_LIBRARY_URL}"
+  "${LIBOVR_LIBRARY_TARGET_DIRECTORY}"
+  "SKIP"
+  )
+
+set(LIBOVR_IMPORT_LOCATION "${CMAKE_BINARY_DIR}/${LIBOVR_LIBRARY_TARGET_DIRECTORY}/LibOVR.${STATIC_LIBRARY_EXTENSION}")
+add_library(LIBOVR_LIBRARY STATIC IMPORTED )
+set_target_properties(LIBOVR_LIBRARY PROPERTIES IMPORTED_LOCATION "${LIBOVR_IMPORT_LOCATION}")
+list(APPEND EXTRA_LIBS LIBOVR_LIBRARY)
+
