12b3879a4305d49518c7da392ffdf1d7552fcccb
James Folk <james.folk@envrmnt.com>
2018-07-25 10:23:07 -0400

    fixed linux build

--- cmake/linux_external_libraries.cmake
+++ cmake/linux_external_libraries.cmake
@@ -3,7 +3,7 @@ message(STATUS "Platform is linux for engine")
 
 find_package(SDL2 REQUIRED)
 list(APPEND EXTRA_LIBS ${SDL2_LIBRARY})
-include_directories(${SDL2_INCLUDE_DIR})
+list(APPEND ${CMAKE_PROJECT_NAME}_THIRDPARTY_INCLUDE_DIRS "${SDL2_INCLUDE_DIR}")
 
 find_package(GLEW REQUIRED)
 list(APPEND EXTRA_LIBS ${GLEW_LIBRARY})
